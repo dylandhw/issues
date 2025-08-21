3ea7ed368502ab582071f5a5035e0f44145ebfda
dylandhw <dylanwilkins2308@gmail.com>
2025-08-20 20:27:58 -0400

    issue struct

--- main.rs
+++ main.rs
@@ -4,3 +4,8 @@ use reqwest::blocking::Client;
 use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, USER_AGENT};
 use serde::Deserialize;
 
+struct Issue {
+    number: u64,
+    title: String,
+    state: String,
+}
